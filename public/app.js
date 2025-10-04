7325e511ee5d697812cdbc61933a4710d22d1af3
DanielLeoPUC <daniel.leo@sga.pucminas.br>
2025-10-04 10:47:32 -0300

    Página Home dinâmica  ajustada e concluida

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+// Cards 
 const obras = [
     { "id": "0", "imagem": "img/avinha.jpg", "titulo": "A Vinha Encarnada", "descricao": " A Vinha Encarnada é um quadro do pintor holandês Vincent Van Gogh, concluído no início de novembro de 1888.<br>&nbsp; Esta foi a única peça vendida pelo artista em vida. O quadro foi adquirido em Bruxelas por 400 Francos, por Anna Boch." },
     { "id": "1", "imagem": "img/giracois.jpg", "titulo": "Girassóis", "descricao": "Van Gogh pintou uma série de 11 quadros de girassóis (quatro em Paris e sete em Arles), que se tornaram algumas das suas obras mais icónicas, simbolizando para ele felicidade, gratidão, força e resistência." },
@@ -8,6 +9,24 @@ const obras = [
 
 ]
 
+//Conteúdo do Aside
+const asd = { "id": "s1", "titulo": "Vincent van Gogh", "descricao": "Vincent van Gogh foi um pintor pós-impressionista holandês (1853-1890) que produziu mais de 2.000 obras, incluindo 900 pinturas, em menos de uma década, mas vendeu apenas uma em vida. Antes de se dedicar à arte aos 27 anos, trabalhou como vendedor de arte, professor e pastor, levando uma vida emocionalmente turbulenta marcada por problemas de saúde mental e pobreza. &nbsp; Após sua morte no anonimato, Van Gogh tornou-se um dos artistas mais influentes da história, conhecido por sua obra vibrante e emotiva, hoje avaliada em milhões de dólares." };
+
+const asideSubdescricoes =
+    [{ "id": "sub1", "subtitulo": "Carreira Artística", "descricao": "Pós-Impressionismo: Van Gogh é um dos principais expoentes do pós-impressionismo." },
+    { "id": "sub2", "subtitulo": "Estilo e Técnica", "descricao": "Sua obra é marcada pelo uso expressivo da cor, beleza e emoção, influenciando a arte do século XX." },
+    { "id": "sub3", "subtitulo": "Reconhecimento Póstumo", "descricao": "Hoje, Van Gogh é celebrado como um dos maiores artistas de todos os tempos, com suas obras exibidas em museus ao redor do mundo, porém o reconhecimento de suas obras vieram após sua morte, e hoje é considerado um dos maiores pintores da história." },
+    { "id": "sub4", "subtitulo": "Producao", "descricao": "Produziu mais de 2.000 obras, incluindo cerca de 900 pinturas, em menos de uma década." },
+    { "id": "sub5", "subtitulo": "Vida Pessoal", "descricao": "Lutou contra problemas de saúde mental, incluindo depressão e psicose, culminando em sua morte por suicídio aos 37 anos." }]
+
+//Conteúdo extra do Aside
+
+const extras =
+    [{ "id": "extra1", "link": "https://pt.wikipedia.org/wiki/Vincent_van_Gogh", "descricao": "Bibliografia" },
+    { "id": "extra2", "link": "https://arteref.com/arte/curiosidades/20-fatos-curiosos-sobre-van-gogh-que-voce-nao-sabia/", "descricao": "Curiosidades" }]
+
+
+//Função para criar os cards dinamicamente
 
 function criarCards() {
     const container = document.getElementById("tela");
@@ -15,17 +34,63 @@ function criarCards() {
 
     for (let i = 0; i < obras.length; i++) {
         var obra = obras[i];
-
-            
         container.innerHTML += `<article class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4  text-center border border-light   " >
     <img src="${obra.imagem}" alt="${obra.titulo}" class="img-fluid">
     <h2>${obra.titulo}</h2>
-    <p>${obra.descricao}</p>&nbsp;
+    <p>${obra.descricao}</p>
     <button class="btn " ">Ver mais</button>
     </article>`;
     }
 }
 
- document.addEventListener("DOMContentLoaded", function () {
-     criarCards();
- });
+
+//Função para criar o Aside dinamicamente
+
+function criarAside() {
+    const asideContainer = document.getElementById("as");
+    asideContainer.innerHTML = "";
+
+
+    asideContainer.innerHTML += `<aside>
+    <div class="col-12">
+    <h1>${asd.titulo}</h1>
+    <p>&nbsp;&nbsp;${asd.descricao}</p>
+     </div>
+     `;
+
+    for (let i = 0; i < asideSubdescricoes.length; i++) {
+        asideContainer.innerHTML += `<div class="col-12">
+        <h4>${asideSubdescricoes[i].subtitulo}</h4>
+        <p>&nbsp;&nbsp;${asideSubdescricoes[i].descricao}</p>
+        </div>
+        `;
+    }
+
+    for (let i = 0; i < extras.length; i++) {
+    asideContainer.innerHTML += 
+    `<nav>
+    <ul class="extras">
+    <li><a href="${extras[i].link}" target="_blank">${extras[i].descricao}</a></li>
+    </ul>
+    </nav></aside>`;
+    }
+
+
+    
+
+
+
+ 
+}
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    criarCards();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    criarAside();
+});
